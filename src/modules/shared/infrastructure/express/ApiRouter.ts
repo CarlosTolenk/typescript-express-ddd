ad9e9c33d318591489e588e639ad175b46e84eee
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-08-15 11:13:00 -0400

    First Commit initialize App

--- src/modules/shared/infrastructure/express/ApiRouter.ts
+++ src/modules/shared/infrastructure/express/ApiRouter.ts
@@ -4,6 +4,10 @@ export const ApiRouter = (): Router => {
     const apiRouter = Router();
     const v1Router = Router();
 
+    apiRouter.get('/', (req, res) => {
+        return res.json({status: ' OK'})
+    })
+
     apiRouter.use('/api/v1', v1Router);
     return apiRouter;
 }
