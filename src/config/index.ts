63a118567a641e22cfd67711c9009d8233eab2c0
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-08-17 14:47:58 -0400

    Add Module TypeOrm

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,11 +11,11 @@ export type Configuration = {
     NODE_ENV: string; PORT: number;
     APP_NAME: string;
     APP_LOG_LEVEL: string;
-    DATABASE_APP_CONTEXT: string
-    DATABASE_APP_HOST: string
-    DATABASE_APP_PORT: number
-    DATABASE_APP_USERNAME: string
-    DATABASE_APP_PASSWORD: string
+    DATABASE_APP_CONTEXT: string;
+    DATABASE_APP_HOST: string;
+    DATABASE_APP_PORT: number;
+    DATABASE_APP_USERNAME: string;
+    DATABASE_APP_PASSWORD: string;
 };
 
 let currentConfig: Configuration = DEVELOPMENT;
